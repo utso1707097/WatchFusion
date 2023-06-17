0f7e67496335d1267e914d0d3ad88400f8d4e0de
utso1707097 <utsomollik1707097@gmail.com>
2023-06-17 22:43:04 +0600

    Filter functionality and channel functionality completed

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -8,9 +8,9 @@ import ChannelDetail from './ChannelDetail'
 
 const VideoCard = ({video:{id:{videoId}, snippet}}) => {
   return (
-    <Card sx={{width:{md:'320px',xs:'100%'},boxShadow:0,borderRadius:'none'}}>
+    <Card sx={{width:{md:'320px',xs:'100%',sm:'358px'},boxShadow:0,borderRadius:'none'}}>
         <Link to={videoId? `/video/${videoId}`:demoVideoUrl}>
-            <CardMedia image={snippet?.thumbnails?.high?.url} alt={snippet?.title} sx={{width : 358,height:180}}/>
+            <CardMedia image={snippet?.thumbnails?.high?.url} alt={snippet?.title} sx={{width : {xs:'100%',sm:'358px',md:'320px'},height:180}}/>
         </Link>
         <CardContent sx={{backgroundColor:"#1e1e1e",height:'106px'}}>
         <Link to={videoId? `/video/${videoId}`:demoVideoUrl}>
